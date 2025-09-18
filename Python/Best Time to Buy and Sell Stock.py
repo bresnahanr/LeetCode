fdb9a53907823eb6418cd32c4276bcbc08b0e4a6
Ryan Bresnahan <ryan.bresn@gmail.com>
2025-09-17 18:43:38 -0600

    "Best time to buy and sell stocks II"

--- Python/Best Time to Buy and Sell Stock.py
+++ Python/Best Time to Buy and Sell Stock.py
@@ -1,3 +1,6 @@
+from math import inf
+
+
 class Solution:
 
     def best_time_to_buy_and_sell_stock(self, prices):
@@ -10,8 +13,24 @@ class Solution:
             min_price = min(min_price, price)
 
         return max_profit
+
+    def best_time_to_buy_and_sell_stock2(self, prices):
+        n = len(prices)
+        buy_prices = inf
+        profit = 0
+        
+        for i in range(n):
+            if prices[i] < buy_prices:
+                buy_prices = prices[i]
+            else:
+                profit += prices[i] - buy_prices
+                buy_prices = prices[i]
+        return profit
     
 prices = [7,1,5,3,6,4]
 sol = Solution()
-ans = sol.best_time_to_buy_and_sell_stock(prices)
-print(ans)
+ans1 = sol.best_time_to_buy_and_sell_stock(prices)
+print(ans1)
+
+ans2 = sol.best_time_to_buy_and_sell_stock2(prices)
+print(ans2)
